bd8418069e0e8a2c4e63068d3311907bfac328b0
Bobach22 <l.user2200@gmail.com>
2020-08-30 18:06:05 +0500

    Boilerplate ready

---
+++ src/Hello.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+
+export function Hello() {
+  return <div>Hello webpack,it starts to tick...</div>;
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Hello } from './Hello';
+import './style/main.less';
+
+ReactDOM.render(<Hello />, document.getElementById('app'));
